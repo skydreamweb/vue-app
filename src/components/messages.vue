c7251216605debaaad0385141300d788c7d5df0c
Dalibor <geneeje@yahoo.com>
2019-11-28 13:08:03 +0100

    Settings Vuetify

--- src/components/messages.vue
+++ src/components/messages.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h4>Messages</h4>
+    <v-list>
+      <v-list-item v-for="message in messages" :key="message.id">
+        <v-list-item-content>
+          <v-list-item-title v-text="message"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
     <ul v-for="message in messages" :key="message.id">
       <li>{{message}}</li>
     </ul>
